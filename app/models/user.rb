2f4c4e6c9d7f364ee2c2e6debadaedd72f8f85a4
cnosuke <shinnosuke@gmail.com>
2015-06-07 01:56:27 +0900

    Add authentication logic using Github

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.uid = auth['uid']
+      user.provider = auth['provider']
+      user.nickname = auth['info']["nickname"]
+      user.name = auth['info']["name"]
+      user.email = auth['info']["email"]
+      user.avatar_url = auth['info']["image"]
+    end
+  end
+end
