3805306cc0c2651174f47233c28aca05cbe38759
cnosuke <shinnosuke@gmail.com>
2015-06-07 02:29:06 +0900

    Add error handlings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
+  class Forbidden < ActionController::ActionControllerError; end
+  include ErrorHandlers
+
   protect_from_forgery with: :exception
   helper_method :current_user
 
   private
   def current_user
     @current_user ||= User.find_by_id(session[:user_id])
+  end
+
+  def check_signin
+    raise "Sign in required." unless current_user
   end
 end
