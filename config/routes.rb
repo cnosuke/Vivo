73135a5d0f7fc7d9ec80815be3ff150c2d40636b
cnosuke <shinnosuke@gmail.com>
2015-06-07 02:29:56 +0900

    Add notes resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-
   root 'pages#index'
 
   get "/auth/:provider/callback" => "sessions#create"
   get "/signout" => "sessions#destroy", :as => :signout
+
+  resources :notes
 end
