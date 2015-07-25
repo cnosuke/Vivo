ccafa2985eb7eb1836e6c8fd3c0c307263ad09a2
cnosuke <shinnosuke@gmail.com>
2015-07-25 23:01:52 +0900

    pending request/notes spec

--- spec/requests/notes_spec.rb
+++ spec/requests/notes_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Notes", type: :request do
-  describe "GET /notes" do
+  let!(:user) { create User }
+  let(:valid_session) { { user_id: user.id } }
+
+  describe "GET /notes", pending: 'あとでなおす' do
     it "works! (now write some real specs)" do
-      get notes_path
+      get notes_path, {}, valid_session
       expect(response).to have_http_status(200)
     end
   end
